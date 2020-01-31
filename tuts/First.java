7f6fbba1ba6b6825cd39af099a7d94034bb7f32e
Zackary Maupin <zmaupin@gmail.com>
2020-01-31 16:20:27 -0500

    initial commit of java repo for coding practice

---
+++ tuts/First.java
@@ -0,0 +1,9 @@
+public class First {
+		public static void main(String args[]) {
+				First e = new First();
+				System.out.println(e.multiply(2, 4));
+		}
+		public int multiply(int a, int b) {
+				return a*b;
+		}
+}
---
+++ tuts/derekbanas/JavaTut/src/HelloWorld.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+import java.util.stream.IntStream;
+
+public class HelloWorld {
+
+	static Scanner sc = new Scanner(System.in);
+
+	final double SHORTPI = 3.14159;
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+//		boolean happy = true;
+//		char a = 'a';
+//		float fNum = 1.111111111111F;
+//		float fNum2 = 1.111111111111F;
+//		System.out.println("Float : " +
+//				(fNum + fNum2));
+//
+//		double dblNum = 1.11111111111111111;
+//		double dblNum2 = 1.1111111111111111;
+//		System.out.println("Double : " + 
+//				(dblNum + dblNum2));
+//
+//		double thousand = 1e+3;
+//		System.out.println(thousand);
+//
+//		long bigNum = 123_456_789;
+
+		// Typecasting
+//		int smInt = 10;
+//		long smLong = smInt;
+//
+//		double cDbl = 1.234;
+//		int cInt = (int) cDbl;
+//		System.out.println(cInt);
+//
+//		long bigLong = 2147486470L;
+//		int bInt = (int) bigLong;
+//		System.out.println(bInt);
+//
+//		String favNum = Double.toString(1.618);
+//// 		Byte.parseByte
+//		int strInt = Integer.parseInt("10");
+//		System.out.println(strInt);
+
+//		System.out.println("5 + 4 = " + (5 + 4));
+//    System.out.println("5 - 4 = " + (5 - 4));
+//    System.out.println("5 * 4 = " + (5 * 4));
+//    System.out.println("5 / 4 = " + (5 / 4));
+//    System.out.println("5 % 4 = " + (5 % 4));
+//    System.out.println("5 / 4 = " + (5.0 % 4.0));
+//
+//		int incMe = 0;
+//		System.out.println("incMe : " + (incMe++));
+//		System.out.println("incMe : " + (++incMe));
+//
+//		incMe += 10;
+
+		// A lot of built-in math functions. abs(), ceil(), floor(), etc.
+		// // Built-in trig functions. log(), sin(), cos().
+
+		// Generate a random number Math.random()
+//		int minNum = 5;
+//		int maxNum = 20;
+//		int randNum = minNum + (int)(Math.random() * 
+//				((maxNum - minNum) + 1));
+//		System.out.println(randNum);
+
+	}
+}
